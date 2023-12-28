dcf99ab6cda34595c5e4ac24a6a4315cdc50522e
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-29 04:52:39 +0700

    * REACT Eat-n-Split - Creating friendlist component - Creating friend component

--- src/App.js
+++ src/App.js
@@ -20,5 +20,44 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <h1>Hello Bandung</h1>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
+}
+
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }

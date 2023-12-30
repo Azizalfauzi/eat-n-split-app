74e31bd333d2a89f8aa3d8bcf3a258d1e61273cf
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-31 06:05:56 +0700

    *React Eat n Split - Communication child to parent - Create funtion handleAddFriend - Handle immutable list of friend -Adding funtion in form addFriend and set init

--- src/App.js
+++ src/App.js
@@ -22,15 +22,21 @@ const initialFriends = [
 ];
 
 export default function App() {
+  const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
   }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "close" : "Add Friend"}
         </Button>
@@ -48,8 +54,7 @@ function Button({ children, onClick }) {
   );
 }
 
-function FriendList() {
-  const friends = initialFriends;
+function FriendList({ friends }) {
   return (
     <ul>
       {friends.map((friend) => (
@@ -81,13 +86,16 @@ function Friend({ friend }) {
   );
 }
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/48?u=499476");
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!name || !image) return;
+
     const id = crypto.randomUUID();
+
     const newFriend = {
       name,
       image: `${image}?=${id}`,
@@ -95,7 +103,10 @@ function FormAddFriend() {
       id: id,
     };
 
-    console.log(newFriend);
+    onAddFriend(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48?u=499476");
   }
   return (
     <form onSubmit={handleSubmit} className="form-add-friend">

5c32c2d9ddf4e6c20d3cb8e8cf22243aca195e55
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-31 05:48:55 +0700

    *React Eat n Split - Adding form add friend with onchange,value and onSubmit - Adding funtion handleSubmit

--- src/App.js
+++ src/App.js
@@ -82,12 +82,35 @@ function Friend({ friend }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48?u=499476");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const id = crypto.randomUUID();
+    const newFriend = {
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+      id: id,
+    };
+
+    console.log(newFriend);
+  }
   return (
-    <form className="form-add-friend">
+    <form onSubmit={handleSubmit} className="form-add-friend">
       <label>🧑‍🤝‍🧑 Friend name :</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label>📷 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
       <Button>Add</Button>
     </form>
   );
